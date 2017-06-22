39b6136052548f1abc2eeb1c703e6f14b2e8b123
ljc2017 <960011268@qq.com>
2017-06-22 15:48:17 +0800

    666

---
+++ js/WeiReading.js
@@ -0,0 +1,122 @@
+$(function(){
+//radio 滑动升框 
+	$(".footer_list").on("tap","li",function(){
+		$(this).addClass("active").siblings().removeClass("active");
+	})
+	$(".radio").on("tap",function(){
+		$(".litre_page").css("height","100%");
+	})
+	$(".del").on("tap",function(){
+		$(".litre_page").css("height","0");
+	})
+//radio 每日更新
+	var d=new Date;
+	var month=d.getMonth()+1; //月
+	var day=d.getDate(); //日
+	var week=d.getDay() //星期
+	var str=["日","一","二","三","四","五","六"]
+	for(var i=0;i<=str.length;i++){
+		if(week==i){
+			week=str[i];
+		}
+	}
+	var wk=week;
+	$(".time").html(month+"月"+day+"日  "+"星期"+wk+" · 每日更新");
+	$(".salltimeed").html(month+"月"+day+"日  ");
+	
+//公开阅读书籍数
+	var len=$(".books_li").length;
+	$(".booknum").html(len+"本公开阅读书籍");
+	
+//书架
+	var selec='<span class="select"></span>';
+	$(".books_li").append($(selec));
+	var z=0;
+	
+//编辑键
+	$(".redact").on("tap",function(){
+		z++;
+		if(z%2==0){
+			$(".select").css("display","none");
+			$(".jump").css("display","none");
+			$(".books_li").find('.select').removeClass("selected");
+		}
+		else{
+			$(".select").css("display","block");
+			$(".books_li").find('.select').removeClass("selected");
+			$(".jump").css("display","block");
+		}
+	})
+	
+//删除键
+	$(".books_li").on("tap",function(){
+		var index=$(this);
+		if($(this).find('.select').hasClass("selected")){
+			$(this).find('.select').removeClass("selected");
+		}
+		else{
+			$(this).find('.select').addClass("selected");
+			$(".jump").on("tap",function(){
+				if($(".books_li").find('.select').hasClass("selected")){
+					index.remove();
+					$(".select").css("display","none");
+					$(".jump").css("display","none");
+					var len=$(".books_li").length;
+					$(".booknum").html(len+"本公开阅读书籍");
+				}
+			})
+		}
+	})
+
+//点击关注
+	$(".gz").on("tap",function(){
+		if($(this).hasClass("on")){
+			$(this).removeClass("on");
+			$(this).text(parseInt($(this).text())-1);
+		}
+		else{
+			$(this).addClass("on");
+			$(this).text(parseInt($(this).text())+1);
+		}
+		
+	})
+
+//start and zant
+	$(".btn").on("tap",function(){
+		if($(this).hasClass("click")){
+			$(this).removeClass("click");
+		}
+		else{
+			$(this).addClass("click");
+		}
+		
+	})
+
+//想法
+	$(".thoughtheader span").on("tap",function(){
+		var index=$(this).index();
+		$(this).addClass("on").siblings("span").removeClass("on");
+		$(".thoughtlist").animate({marginLeft:-index*320+"px"},300);
+	})
+	
+//nav
+	$(".nav").eq(0).css("display","block").siblings().css("display","none");
+	$(".footer_list li").on("tap",function(){
+		var index=$(this).index();
+		$(".nav").eq(index).css("display","block").siblings().css("display","none");
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+})
---
+++ js/WeiReadingjs.js
@@ -0,0 +1,77 @@
+function  getTimeFormat(Num){
+	var m=parseInt(Num%3600/60);
+	if(m<10){            //最常用的方法 if判断
+		m="0"+m;
+	}
+	var s=parseInt(Num%60);
+	s<10 && (s="0"+s);   //短路0p0
+	return m+":"+s;
+}
+var ado=document.getElementById("myaudio");
+var pdt=document.getElementById("proceedtime");
+var plt=document.getElementById("playtime");
+var btn=document.getElementById("btn");
+var left=document.getElementById("left");
+var right=document.getElementById("right");
+var pB=document.getElementById("prograssBar");
+var sbtn=document.getElementById("schedule_btn");
+
+//setInterval(function(){
+//	pdt.innerHTML=getTimeFormat(ado.currentTime);
+//	plt.innerHTML=getTimeFormat(ado.duration);
+////	rng.value=ov.currentTime/ov.duration;
+//},1000)				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+//				btn.onclick=function(){
+//					
+//					if(ov.paused)
+//					{
+//						ov.play();
+//						this.innerHTML="停止";
+//					}
+//					else{
+//						ov.pause();
+//						this.innerHTML="播放";
+//					}
+//					 
+//				}
+//				
+//				ov.addEventListener("ended",function(){
+//					 if(confirm("是否要重播?")){
+//					 	ov.play();
+//					 }
+//				})
+//				
+//				rng.onchange=function(){
+//					 ov.currentTime=ov.duration*this.value;
+//				}
+//				//setTimeout(()=>alert(ov.duration),2000);
+//				window.onunload=function(){
+//					  localStorage.setItem("vpos",ov.currentTime);
+//				}
+//				if(localStorage.getItem("vpos")){
+//					var p=localStorage.getItem("vpos")
+//					 ov.currentTime=p;
+//				}
